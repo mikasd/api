9545afd4d3098e06e477479afe1b1ea37cb16ff0
mike <michael.ingley@gmail.com>
2021-07-17 21:26:59 -0700

    adds testing dev dependencies and sets up basic first unit test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,20 @@
-use skel::run;
+#[actix_rt::test]
+async fn health_check_works() {
+    spawn_app().await.expect("Failed to spawn app");
 
-#[test]
-fn dummy_test(){
-    run()
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://localhost:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to get health check");
+
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+async fn spawn_app() -> () {
+    todo!()
 }

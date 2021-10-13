cbaded8f041a1a48b90ca458c0edc7104a5368f4
mingley <54518@icf.com>
2021-10-13 05:54:35 -0700

    adds serde, creates form data struct, implements subscriptions route and handler, adds new tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -18,6 +18,55 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[actix_rt::test]
+async fn subscribe_returns_200_on_valid_form_data() {
+    // arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // act
+    let response = client.post(&format!("{}/subscriptions", address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("request failed.");
+
+    // assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_missing() {
+    // arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // act
+        let response = client.post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Request failed to execute");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "Api did not fail with 400 status when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed binding to a random port");

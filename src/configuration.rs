3ac05b73989d1eb50c59ceba86b4f436cbbb1bde
mingley <54518@icf.com>
2021-10-18 11:13:34 -0700

    adds get_configuration fn to look for config file and extract settings from

--- src/configuration.rs
+++ src/configuration.rs
@@ -12,3 +12,14 @@ pub struct DatabaseSettings {
     pub host: String,
     pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    //init cfg reader
+    let mut settings = config::Config::default();
+
+    //add cfg vals from file named configuration
+    //crate will look for any top level file w parse-able extension yaml/json/etc
+    settings.merge(config::File::with_name("configuration"))?;
+    //try to convert cfg vals into settings type
+    settings.try_into()
+}
